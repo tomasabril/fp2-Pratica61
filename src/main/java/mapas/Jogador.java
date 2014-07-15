194d721581889b9b48f885752e778126817e1ad2
a1013343 <a1013343@Einherjar>
2014-07-15 11:44:23 -0300

    adicionando arquivos

---
+++ src/main/java/mapas/Jogador.java
@@ -0,0 +1,20 @@
+package mapas;
+
+/**
+ *
+ * @author Tomas Abril
+ */
+public class Jogador {
+    private int numero;
+    private String nome;
+
+    public Jogador(int numero, String nome) {
+        this.numero = numero;
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return numero + " - " + nome;
+    }
+}

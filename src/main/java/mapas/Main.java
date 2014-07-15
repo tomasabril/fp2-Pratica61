156053d2f32854874c23140d94db0c8e3816afd1
a1013343 <a1013343@Einherjar>
2014-07-15 11:49:47 -0300

    arrumei formatacao da saida

--- src/main/java/mapas/Main.java
+++ src/main/java/mapas/Main.java
@@ -16,18 +16,18 @@ public class Main
     public static void main(String[] args) 
     {
         HashMap<String, Jogador> time1 = new HashMap<>();
-        time1.put("Goleiro ", new Jogador(1, "Julius Caesar"));
-        time1.put("Atacante", new Jogador(10, "Newymeier"));
-        time1.put("Zagueiro", new Jogador(7, "Thiago Silva"));
+        time1.put("Goleiro ", new Jogador(1,  "Julius Caesar "));
+        time1.put("Atacante", new Jogador(10, "Newymeier    "));
+        time1.put("Zagueiro", new Jogador(7,  "Thiago Silva  "));
         
         HashMap<String, Jogador> time2 = new HashMap<>();
-        time2.put("Goleiro ",  new Jogador(1, "Nóiado"));
+        time2.put("Goleiro ", new Jogador(1,  "Nóiado         "));
         time2.put("Atacante", new Jogador(10, "Schwarzenegger"));
-        time2.put("Zagueiro", new Jogador(7, "Hummels"));
+        time2.put("Zagueiro", new Jogador(7,  "Hummels        "));
         
         Set<String> timeset = time1.keySet();
        
-        System.out.println("           time 1" + "                time 2");
+        System.out.println("           time 1" + "               time 2");
         for(String posicao: timeset)
         {
             System.out.println(posicao + " : " + time1.get(posicao) + " : " + time2.get(posicao));
